c8c0226b8ac7ec3bd1afd68fe762527bec8a76b8
ahmaddo <a.ahmad@outlook.de>
2017-04-25 23:27:16 +0200

    gulpfile extended

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,26 @@ const DESTINATION = 'dest/';
 
 gulp.task('copy', function(){
     return gulp
-        .src('app/*/**')
-        .pipe(gulp.dest(DESTINATION));
+        .src('app/*')
+        .pipe(gulp.dest(DESTINATION + 'tests'));
 })
-    .task('default', ['copy']);
+    .task('run', function(){
+        return gulp
+            .src('app/class/*')
+            .pipe(gulp.dest(DESTINATION + 'tests/class'));
+    })
+
+    .task('vendor', function(){
+        return gulp
+            .src(['vendor/*', 'vendor/*'])
+            .pipe(gulp.dest(DESTINATION + 'vendor'));
+    })
+
+    .task('tests', function(){
+        return gulp
+            .src('app/tests/*/**')
+            .pipe(gulp.dest(DESTINATION));
+    })
+
+    .task('default', [ 'run', 'copy', 'vendor', 'tests']);
 
